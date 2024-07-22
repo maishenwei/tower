cdf5cbeb2d13310efdc7232ccaa9c551e2ae0581
maishenwei <75930284+maishenwei@users.noreply.github.com>
2024-07-22 22:37:21 +0800

    建立文件夹，包含热更新，单例基类，初始化main.lua

---
+++ Unity/Assets/Scripts/Lua/LuaMgr.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XLua;
+using System.IO;
+
+
+public class LuaMgr : SingletonBase<LuaMgr>
+{
+    private LuaEnv luaEnv;
+
+    public void Init()
+    {
+        luaEnv = new LuaEnv();
+        luaEnv.AddLoader(CustomLoader);
+
+        luaEnv.DoString("require('main')");
+    }
+
+    private byte[] CustomLoader(ref string filePath)
+    {
+        //通过函数中的逻辑 去加载lua文件
+
+        //传入的参数是 require执行的Lua脚本文件名
+        Debug.Log(filePath);
+
+        //拼接一个lua文件所在路径
+        //Application.dataPath 可以得到Assets路径 
+        //记得加上lua后缀
+        string path = Application.dataPath + "/Lua/" + filePath + ".lua";
+
+        //有路径 就去加载文件
+        //File知识点 C#提供的文件读写的类
+        //需要 System.IO命名空间
+        //判断文件是否存在
+        if (File.Exists(path))
+        {
+            return File.ReadAllBytes(path);
+
+        }
+        else
+        {
+            Debug.Log("不存在lua文件：" + filePath);
+        }
+
+
+        return null;
+    }
+}
---
+++ Unity/Assets/Scripts/Main.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        LuaMgr.Instance.Init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+  
+}
